56a0b75754f128e95972d1e37978aca6d28fbb4e
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-04-07 16:15:06 -0300

    banner y social components

--- proyectoFinalv3/src/app/app.module.ts
+++ proyectoFinalv3/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgramaComponent } from './components/logo-arg-programa/logo-arg-programa.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoArgProgramaComponent
+    LogoArgProgramaComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
