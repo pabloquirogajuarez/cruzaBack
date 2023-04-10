e98a256cfefc295b5cb1a4c73353ca3b3f84324e
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-04-10 00:54:51 -0300

    update controllers

--- pBack/src/main/java/com/portfolio/pabloquirogajuarez/Controller/PersonaController.java
+++ pBack/src/main/java/com/portfolio/pabloquirogajuarez/Controller/PersonaController.java
@@ -3,6 +3,56 @@
 package com.portfolio.pabloquirogajuarez.Controller;
 
 
-public class PersonaController {
 
+import com.portfolio.pabloquirogajuarez.Entity.Persona;
+import com.portfolio.pabloquirogajuarez.Interface.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//indicar a la base de datos que es un controllador
+@RestController
+public class PersonaController {
+    @Autowired IPersonaService ipersonaService;
+    
+    //el front diferencia depende la url, el tipo de metodo que va a utilizar
+    @GetMapping("personas/traer")
+    public List<Persona> getPersona(){
+        return ipersonaService.getPersona();
+    }
+    
+    //el front guarda esto en la base de datos.
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "Persona creada correctamente";
+    }
+    
+    @DeleteMapping("/personas/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        ipersonaService.deletePersona(id);
+        return "Persona eliminada correctamente";
+    }
+    //URL:PUERTO/personas/editar/idUsuario/nombre&apellido&img
+    @PutMapping("/personas/editar/{id}")
+    public Persona editPersona(@PathVariable Long id,
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg){
+        Persona persona = ipersonaService.findPersona(id);
+        
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+        
+        ipersonaService.savePersona(persona);
+        return persona;
+    }
 }
