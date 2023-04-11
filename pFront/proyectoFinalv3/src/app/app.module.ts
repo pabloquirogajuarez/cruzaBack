80c1438d9dff3ce0fbed703e4b644299d05436da
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-04-11 17:50:10 -0300

    Fix app.module.ts
    Faltaba un modulo en app.components.ts

--- pFront/proyectoFinalv3/src/app/app.module.ts
+++ pFront/proyectoFinalv3/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -36,7 +37,8 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
