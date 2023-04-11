52308085a8328f908c0eb77b6776366afe828620
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-04-11 00:56:31 -0300

    ruteo de home y login

--- pFront/proyectoFinalv3/src/app/app.module.ts
+++ pFront/proyectoFinalv3/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClientModule } from '@angular/common/http';
     EduComponent,
     SkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
