3790bf8cdb7eb7d1a1cc92c81693474eeadeb893
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-04-11 01:26:44 -0300

    .

--- pFront/proyectoFinalv3/src/app/components/logo-arg-programa/logo-arg-programa.component.ts
+++ pFront/proyectoFinalv3/src/app/components/logo-arg-programa/logo-arg-programa.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-arg-programa',
   templateUrl: './logo-arg-programa.component.html',
   styleUrls: ['./logo-arg-programa.component.css']
 })
-export class LogoArgProgramaComponent {
+export class LogoArgProgramaComponent implements OnInit{
+  constructor(private router:Router) {}
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
+
 
 }
