5f94804c7843ca74d9992f0ca6afc315dc448827
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-04-20 04:29:37 -0300

    fix

--- pFront/proyectoFinalv3/src/app/components/edu/edu.component.ts
+++ pFront/proyectoFinalv3/src/app/components/edu/edu.component.ts
@@ -1,45 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Edu } from 'src/app/model/edu';
-import { EduService } from 'src/app/service/edu.service';
-import { TokenService } from 'src/app/service/token.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edu',
   templateUrl: './edu.component.html',
   styleUrls: ['./edu.component.css']
 })
-export class EduComponent implements OnInit {
-  edu: Edu[] = [];
+export class EduComponent {
 
-  constructor(private eduS: EduService, private tokenService: TokenService) { }
-  isLogged = false;
-
-  ngOnInit(): void {
-    this.cargarEdu();
-    if(this.tokenService.getToken()){
-      this.isLogged = true;
-    } else {
-      this.isLogged = false;
-    }
-  }
-
-  cargarEdu(): void{
-    this.eduS.lista().subscribe(
-      data =>{
-        this.edu = data;
-      }
-    )
-  }
-
-  delete(id?: number){
-    if( id != undefined){
-      this.eduS.delete(id).subscribe(
-        data => {
-          this.cargarEdu();
-        }, err => {
-          alert("No se pudo eliminar");
-        }
-      )
-    }
-  }
 }
