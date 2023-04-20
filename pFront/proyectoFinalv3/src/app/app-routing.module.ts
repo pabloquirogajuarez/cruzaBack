4380082e138b15b3349f7057a3a1d90766e48a66
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-04-20 05:33:02 -0300

    edu y exp agregados correctamente

--- pFront/proyectoFinalv3/src/app/app-routing.module.ts
+++ pFront/proyectoFinalv3/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/edu/neweducacion.component';
+import { EditeducacionComponent } from './components/edu/editeducacion.component';
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path:'nuevaexp', component: NewExperienciaComponent},
   {path:'editexp', component: EditExperienciaComponent},
   {path:'editexp/:id', component: EditExperienciaComponent},
-  {path:'nuevaedu', component: NeweducacionComponent}
+  {path:'nuevaedu', component: NeweducacionComponent},
+  {path:'editedu/:id', component: EditeducacionComponent}
 ];
 
 @NgModule({
--- pFront/proyectoFinalv3/src/app/app.module.ts
+++ pFront/proyectoFinalv3/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/edu/neweducacion.component';
+import { EditeducacionComponent } from './components/edu/editeducacion.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { NeweducacionComponent } from './components/edu/neweducacion.component';
     LoginComponent,
     NewExperienciaComponent,
     EditExperienciaComponent,
-    NeweducacionComponent
+    NeweducacionComponent,
+    EditeducacionComponent
   ],
   imports: [
     BrowserModule,
