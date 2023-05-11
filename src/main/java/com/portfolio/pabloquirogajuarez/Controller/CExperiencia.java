8af246613614d62112685943d1a93ccb2d8b8f66
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-05-11 08:22:31 -0300

    fix crossorigin v7 en local funciona.

--- src/main/java/com/portfolio/pabloquirogajuarez/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/pabloquirogajuarez/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://frontendpqj.web.app")
+@CrossOrigin(origins = {"https://frontendpqj.web.app","http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
