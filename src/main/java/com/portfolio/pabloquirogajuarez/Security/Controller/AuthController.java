52aefca025b2dfafbbd4d51b123d390b9d2fdf39
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-05-11 05:55:21 -0300

    controller crossorigin update

--- src/main/java/com/portfolio/pabloquirogajuarez/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/pabloquirogajuarez/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendpqj.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
