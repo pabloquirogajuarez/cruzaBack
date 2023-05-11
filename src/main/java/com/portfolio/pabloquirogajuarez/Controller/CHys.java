f655ab59527ea2123df25d57201e145fefebb227
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-05-11 07:55:18 -0300

    crossorigin v5

--- src/main/java/com/portfolio/pabloquirogajuarez/Controller/CHys.java
+++ src/main/java/com/portfolio/pabloquirogajuarez/Controller/CHys.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = {"https://frontendpqj.web.app"})
+@CrossOrigin(origins = "https://frontendpqj.web.app")
 
 @RequestMapping("/skill")
 public class CHys {
