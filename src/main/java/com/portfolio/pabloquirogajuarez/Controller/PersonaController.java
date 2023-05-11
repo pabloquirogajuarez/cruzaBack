2d771ae8e97dec069a9d02ca26ffa8b814dcf052
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-05-11 05:23:53 -0300

    crossorigin frontonline

--- src/main/java/com/portfolio/pabloquirogajuarez/Controller/PersonaController.java
+++ src/main/java/com/portfolio/pabloquirogajuarez/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendpqj.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
