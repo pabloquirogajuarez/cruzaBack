1b987aace1caea25bc9fed96f3f9330212d24290
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-05-23 00:44:00 -0300

    documentacion en swagger

--- src/main/java/com/portfolio/pabloquirogajuarez/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/pabloquirogajuarez/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontendpqj.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendpqj.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
