82ac0d74d7c186791cc9075d681a49b7258b8b49
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-05-23 00:48:30 -0300

    documentacion en swagger

--- src/main/java/com/portfolio/pabloquirogajuarez/Controller/CHys.java
+++ src/main/java/com/portfolio/pabloquirogajuarez/Controller/CHys.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = {"https://frontendpqj.web.app"})
+@CrossOrigin(origins = {"https://frontendpqj.web.app","http://localhost:4200"})
 
 @RequestMapping("/skill")
 public class CHys {
