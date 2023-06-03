825ac78b0baf5fcebaf127798e8dbcc1b36ee5a6
pablo <44243960+pabloquirogajuarez@users.noreply.github.com>
2023-06-03 17:57:31 -0300

    link security actualizado

--- src/main/java/com/cruza/Security/Controller/AuthController.java
+++ src/main/java/com/cruza/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontendpqj.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://cruzafront.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
